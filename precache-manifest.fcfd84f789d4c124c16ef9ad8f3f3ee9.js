eb29c4ac6980186f18d785e1db25e0115ec48456
Development <development@MacBook-Pro.local>
2019-02-01 16:00:07 +0100

    Updates

---
+++ precache-manifest.fcfd84f789d4c124c16ef9ad8f3f3ee9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3d8a7cf696e63b6179b",
+    "url": "/appstore/static/js/runtime~main.e3d8a7cf.js"
+  },
+  {
+    "revision": "3eb3e757def6c1b4be1a",
+    "url": "/appstore/static/js/main.3eb3e757.chunk.js"
+  },
+  {
+    "revision": "a49e266e23ed945c8f3b",
+    "url": "/appstore/static/js/1.a49e266e.chunk.js"
+  },
+  {
+    "revision": "3eb3e757def6c1b4be1a",
+    "url": "/appstore/static/css/main.b6547175.chunk.css"
+  },
+  {
+    "revision": "a8556ccd1127846d75878b6d2f87d705",
+    "url": "/appstore/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/appstore/precache-manifest.0bd6b2b27760471061c10592dced6ed0.js"
+  "/appstore/precache-manifest.fcfd84f789d4c124c16ef9ad8f3f3ee9.js"
 );
 
 workbox.clientsClaim();
