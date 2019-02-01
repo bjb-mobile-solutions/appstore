42dc7a6873340f75811de157d7e1cb150ec172de
Development <development@MacBook-Pro.local>
2019-02-01 14:24:22 +0100

    Updates

---
+++ precache-manifest.36c663943cf6efac1f2c99bd346cd811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3d8a7cf696e63b6179b",
+    "url": "/appstore/static/js/runtime~main.e3d8a7cf.js"
+  },
+  {
+    "revision": "18642a6061df2c32f8c6",
+    "url": "/appstore/static/js/main.18642a60.chunk.js"
+  },
+  {
+    "revision": "00b5649620f7529ca676",
+    "url": "/appstore/static/js/1.00b56496.chunk.js"
+  },
+  {
+    "revision": "18642a6061df2c32f8c6",
+    "url": "/appstore/static/css/main.934de6a3.chunk.css"
+  },
+  {
+    "revision": "96e785b09a0d4a6e0b8d5c54e4d16376",
+    "url": "/appstore/index.html"
+  }
+];
