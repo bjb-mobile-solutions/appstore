2aff41e228f312267aecfa45acd5000678467119
Development <development@MacBook-Pro.local>
2019-02-01 15:37:32 +0100

    - updated route path

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import './App.css';
 import AppStore from './components/appstore/AppStore';
 import DeviceList from './components/devices//DeviceList';
 
-
-
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           <div className="App-header">
             <h1>Julius Baer</h1>
             <p>Internal App Store</p>
           </div>
           <div className="App-content">
+            <Switch>
+              <Route exact path='/' component={AppStore} />
+              <Route path='/devices' component={DeviceList} />
+            </Switch>
           </div>
-          <Route exact path="/" component={AppStore} />
-          <Route path="/devices" component={DeviceList} />
         </div>
-      </Router >
+      </Router>
     );
   }
 }
