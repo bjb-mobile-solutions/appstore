deaca7a9433fe222922043c9e239af99b81c3609
Development <development@Rolands-MacBook-Pro.local>
2019-03-22 15:08:40 +0100

    - removed old app versions

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
               </div>}
-            <p>version: 3.0.1</p>
+            <p>version: 3.0.2</p>
           </footer>
         </div>
       </Router >
