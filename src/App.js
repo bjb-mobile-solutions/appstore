57c7d4d1f6de41266f000f723dd0d39a64be83b1
Development <development@Rolands-MacBook-Pro.local>
2019-04-01 12:46:00 +0200

    - fix

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
               </div>}
-            <p>version: 3.0.7</p>
+            <p>version: 3.0.8</p>
           </footer>
         </div>
       </Router >
