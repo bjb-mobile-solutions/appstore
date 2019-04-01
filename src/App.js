3959e563ea7f6dd67d13d3a2e26f2d2f5f0036bc
Development <development@Rolands-MacBook-Pro.local>
2019-04-01 09:32:23 +0200

    - adde new app version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
               </div>}
-            <p>version: 3.0.5</p>
+            <p>version: 3.0.6</p>
           </footer>
         </div>
       </Router >
