7f47fa23d6f0c6ff41f14044e7147ca2262d6e74
Development <development@Rolands-MacBook-Pro-2.local>
2019-05-20 09:29:27 +0200

    - version bump

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
             </div>}
-            <p>version: 4.0.0</p>
+            <p>version: 4.0.1</p>
           </footer>
         </div>
       </Router >
