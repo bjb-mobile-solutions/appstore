54d5306cb823622360eacb955da8422814132241
Mobile Competence Center 02 <mobilecompetencecenter02@MacBook-Pro.local>
2019-06-13 08:28:19 +0200

    update

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
             </div>}
-            <p>version: 4.0.7</p>
+            <p>version: 4.0.8</p>
           </footer>
         </div>
       </Router >
