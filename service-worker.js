60431d6b18e3f54b78ba4c2a1fb918743053025a
Mobile Competence Center 02 <mobilecompetencecenter02@MacBook-Pro.local>
2019-06-13 08:27:15 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/appstore/precache-manifest.b488aa775ddff9a11430b0116b71213a.js"
+  "/appstore/precache-manifest.71bbf75428919d4afcebdd58516508dd.js"
 );
 
 workbox.clientsClaim();
