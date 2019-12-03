559a01e488c6edf213502394851479f690569bca
Development <development@Rolands-MacBook-Pro-2.local>
2019-12-03 16:02:36 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/appstore/precache-manifest.5dbb464277c5ac99857850e77f9b7c37.js"
+  "/appstore/precache-manifest.3105d49e76a9d91bffe4f25093c141a9.js"
 );
 
 workbox.clientsClaim();
