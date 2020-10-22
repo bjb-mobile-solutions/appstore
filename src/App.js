f4a7eaeb404c94115ff696ce4dab778d7173652c
Roland Michelberger <roland.michelberger@juliusbaer.com>
2020-10-22 09:17:47 +0200

    - clean up

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
             </div>}
-            <p>version: 4.4.1</p>
+            <p>version: 4.4.3</p>
           </footer>
         </div>
       </Router >
