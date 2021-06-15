ba2ee4e71ae89484a8f784ca5e36037b2c34d711
Roland Michelberger <roland.michelberger@juliusbaer.com>
2021-06-15 08:31:41 +0200

    - new version

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
             </div>}
-            <p>version: 4.6.8</p>
+            <p>version: 4.6.9</p>
           </footer>
         </div>
       </Router >
