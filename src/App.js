e3f8a8da51365c06d01d47324dd7e25df0f8fb47
Mobile Competence Center 02 <mobilecompetencecenter02@MacBook-Pro.sunrise.box>
2021-11-05 15:53:14 +0100

    -new version

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
             </div>}
-            <p>version: 4.6.11</p>
+            <p>version: 4.6.12</p>
           </footer>
         </div>
       </Router >
