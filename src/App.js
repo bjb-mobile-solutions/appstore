6eb25c0ebec44e1347a44d8835a7ee63785134f6
Roland Michelberger <roland.michelberger@juliusbaer.com>
2022-02-28 16:27:11 +0100

    Prepare release 5.0

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
             </div>}
-            <p>version: 4.9</p>
+            <p>version: 5.0</p>
           </footer>
         </div>
       </Router >
