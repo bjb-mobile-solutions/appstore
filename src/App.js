ee660838d89f2255386488a4015ae60c8928b1b7
Patryk Zabicki <patryk.zabicki@juliusbaer.com>
2022-10-03 10:13:20 +0200

    updating with new profile

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
             </div>}
-            <p>version: 5.3</p>
+            <p>version: 5.4</p>
           </footer>
         </div>
       </Router >
