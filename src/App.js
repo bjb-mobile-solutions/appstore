1af17e2c1d3831f2bca68ed6e8d39f4d471323ed
Patryk Zabicki <patryk.zabicki@juliusbaer.com>
2022-10-21 11:08:10 +0200

    fix path for rc biulds ios

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
             </div>}
-            <p>version: 5.5.0</p>
+            <p>version: 5.5.1</p>
           </footer>
         </div>
       </Router >
