6fbc65be211e1039a296858c500f124a0ab2eeb8
Development <channels-cc-mobile@juliusbaer.com>
2023-05-30 09:36:26 +0200

    deployment for RC for 3.25.9

--- src/App.js
+++ src/App.js
@@ -68,8 +68,8 @@ class App extends Component {
               <p>Signed in as: {this.state.authUser.email}</p>
               <SignOutButton />
             </div>}
-            <p>version: 5.8</p>
-              <p>Last change: uploaded RC for 3.11.66 and 67.</p>
+            <p>version: 5.9</p>
+              <p>Last change: uploaded RC for 3.25.9.</p>
           </footer>
         </div>
       </Router >
